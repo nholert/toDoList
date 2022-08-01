54f6c0abc9cdbb08f1376581f6d04abad7e681ea
Natalie <nataliemholert@gmail.com>
2022-08-01 10:23:34 -0400

    Initial Commit

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Scholar on 7/29/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name: String = ""
+    var important : Bool = false
+    
+}
+
+
