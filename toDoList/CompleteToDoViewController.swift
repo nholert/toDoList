10544c773c291228488e86bc0f42618f4016441e
Natalie <nataliemholert@gmail.com>
2022-08-02 10:12:51 -0400

    Iteration 1

---
+++ toDoList/CompleteToDoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CompleteToDoList.swift
+//  toDoList
+//
+//  Created by Scholar on 8/2/22.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+    var previousVC = toDoTableViewController()
+    var selectedToDo = ToDo()
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = selectedToDo.name
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- toDoList/toDoTableViewController.swift
+++ toDoList/toDoTableViewController.swift
@@ -65,6 +65,13 @@ class toDoTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+      // this gives us a single ToDo
+      let toDo = toDos[indexPath.row]
+
+      performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -106,11 +113,19 @@ class toDoTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      if let addVC = segue.destination as? AddToDoViewController {
-        addVC.previousVC = self
-      }
-    }
 
+
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+  if let addVC = segue.destination as? AddToDoViewController {
+    addVC.previousVC = self
+  }
+
+    if let completeVC = segue.destination as? CompleteToDoViewController {
+    if let toDo = sender as? ToDo {
+      completeVC.selectedToDo = toDo
+      completeVC.previousVC = self
+    }
+  }
 }
 
+} 
